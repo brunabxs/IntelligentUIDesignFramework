b5ac9230b7b52281918658652a9c345ab219d4a6
Bruna Xavier <brunabxsantos@gmail.com>
2014-09-15 22:41:44 -0300

    Refactor na classe de teste CrossoverMethodTest

--- tests/CrossoverMethodTest.php
+++ tests/CrossoverMethodTest.php
@@ -2,86 +2,91 @@
 include_once 'MyUnit_Framework_TestCase.php';
 class CrossoverMethodTest extends MyUnit_Framework_TestCase
 {
-  public function testSimple_twoIndividuals_genomeSize3_point1()
+  public function testSimple_point1_mustReturnTwoIndividualsWithGenomeDifferentFromParents()
   {
-    $ga = $this->getMockBuilder('GeneticAlgorithm')
-               ->disableOriginalConstructor()
-               ->getMock();
+    // Arrange
+    $ga = $this->mockGeneticAlgorithm();
     $ga->genomeSize = 3;
-
     $individual1 = new Individual($ga, '000');
     $individual2 = new Individual($ga, '111');
-
     CrossoverMethod::$prob = 1;
+
+    // Act
     $newIndividuals = CrossoverMethod::simple($ga, $individual1, $individual2, 1);
+
+    // Assert
     $this->assertEquals(2, count($newIndividuals));
     $this->assertEquals('001', $newIndividuals[0]->genome);
     $this->assertEquals('110', $newIndividuals[1]->genome);
   }
 
-  public function testSimple_twoIndividuals_genomeSize3_point0()
+  public function testSimple_point0_mustReturnTwoIndividualsWithGenomeDifferentFromParents()
   {
-    $ga = $this->getMockBuilder('GeneticAlgorithm')
-               ->disableOriginalConstructor()
-               ->getMock();
+    // Arrange
+    $ga = $this->mockGeneticAlgorithm();
     $ga->genomeSize = 3;
-
     $individual1 = new Individual($ga, '000');
     $individual2 = new Individual($ga, '111');
-
     CrossoverMethod::$prob = 1;
+
+    // Act
     $newIndividuals = CrossoverMethod::simple($ga, $individual1, $individual2, 0);
+
+    // Assert
     $this->assertEquals(2, count($newIndividuals));
     $this->assertEquals('011', $newIndividuals[0]->genome);
     $this->assertEquals('100', $newIndividuals[1]->genome);
   }
 
-  public function testSimple_twoIndividuals_genomeSize3_point2()
+  public function testSimple_point2_mustReturnTwoIndividualsWithGenomeEqualsToParents()
   {
-    $ga = $this->getMockBuilder('GeneticAlgorithm')
-               ->disableOriginalConstructor()
-               ->getMock();
+    // Arrange
+    $ga = $this->mockGeneticAlgorithm();
     $ga->genomeSize = 3;
-
     $individual1 = new Individual($ga, '000');
     $individual2 = new Individual($ga, '111');
-
     CrossoverMethod::$prob = 1;
+
+    // Act
     $newIndividuals = CrossoverMethod::simple($ga, $individual1, $individual2, 2);
+
+    // Assert
     $this->assertEquals(2, count($newIndividuals));
     $this->assertEquals('000', $newIndividuals[0]->genome);
     $this->assertEquals('111', $newIndividuals[1]->genome);
   }
 
-  public function testSimple_twoIndividuals_genomeSize3_noPoint()
+  public function testSimple_noPoint_mustReturnTwoIndividuals()
   {
-    $ga = $this->getMockBuilder('GeneticAlgorithm')
-               ->disableOriginalConstructor()
-               ->getMock();
+    // Arrange
+    $ga = $this->mockGeneticAlgorithm();
     $ga->genomeSize = 3;
-
     $individual1 = new Individual($ga, '000');
     $individual2 = new Individual($ga, '111');
-
     CrossoverMethod::$prob = 1;
+
+    // Act
     $newIndividuals = CrossoverMethod::simple($ga, $individual1, $individual2);
+
+    // Assert
     $this->assertEquals(2, count($newIndividuals));
     $this->assertEquals(3, strlen($newIndividuals[0]->genome));
     $this->assertEquals(3, strlen($newIndividuals[1]->genome));
   }
 
-  public function testSimple_twoIndividuals_genomeSize3_noCrossover()
+  public function testSimple_noCrossover_mustReturnSameIndividuals()
   {
-    $ga = $this->getMockBuilder('GeneticAlgorithm')
-               ->disableOriginalConstructor()
-               ->getMock();
+    // Arrange
+    $ga = $this->mockGeneticAlgorithm();
     $ga->genomeSize = 3;
-
     $individual1 = new Individual($ga, '000');
     $individual2 = new Individual($ga, '111');
-
     CrossoverMethod::$prob = 0;
+
+    // Act
     $newIndividuals = CrossoverMethod::simple($ga, $individual1, $individual2);
+
+    // Assert
     $this->assertEquals(2, count($newIndividuals));
     $this->assertEquals(3, strlen($newIndividuals[0]->genome));
     $this->assertEquals('000', $newIndividuals[0]->genome);
