6b3293660ad75b0aa909565b24524d18c002088f
Bruna Xavier <brunabxsantos@gmail.com>
2014-09-18 15:14:19 -0300

    Adicionando evento a Piwik (ao clicar num link, por exemplo)

--- client/public/js/default.js
+++ client/public/js/default.js
@@ -1,1 +1,9 @@
+jQuery(document).ready(function() {
+  jQuery('a').click(function() {
+    trackEvent('click', 1);
+  });
+});
 
+var trackEvent = function(name, value) {
+  _paq.push(['trackEvent', 'GA', __AppConfig.generation + '.' + __AppConfig.genome, name, value]);
+}
