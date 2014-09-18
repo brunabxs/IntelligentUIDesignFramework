2dc5fae2b4abd95c4b402690ffd3d19d54717db9
Bruna Xavier <brunabxsantos@gmail.com>
2014-09-18 12:17:57 -0300

    Retornando informação do indivíduo

--- server/script.php
+++ server/script.php
@@ -8,7 +8,7 @@ spl_autoload_register('autoload');
 
 $dir = './resources/';
 
-$currentGeneration = GeneticAlgorithm::getLastGeneration();
+$currentGeneration = GeneticAlgorithm::getLastGeneration($dir);
 
 $jsonString = file_get_contents($dir . $currentGeneration . '-GA.json');
 $jsonString = preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $jsonString);
@@ -19,7 +19,5 @@ $individuals = json_decode($jsonString)->individuals;
 $individualIndex = rand(0, count($individuals) - 1);
 $individualGenome = $individuals[$individualIndex];
 
-echo $dir . $currentGeneration . '-' . $individualIndex . '-' . $individualGenome . '.json';
-
-//echo file_get_contents($dir . $currentGeneration . '-' . $individualIndex . '-' . $individualGenome . '.json');
+echo file_get_contents($dir . $currentGeneration . '-' . $individualIndex . '-' . $individualGenome . '.json');
 ?>
