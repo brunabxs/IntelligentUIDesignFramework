fab9d288870a3c886797de3a6e6a4a5996664319
Bruna Xavier <brunabxsantos@gmail.com>
2014-09-19 01:07:49 -0300

    Criando nova população do algoritmo genético

--- server/startNewPopulation.php
+++ server/startNewPopulation.php
@@ -21,12 +21,31 @@ else
 
 $jsonString = preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $jsonString);
 $jsonString = trim($jsonString);
-$ga = new GeneticAlgorithm(json_decode($jsonString), $dir);
+$ga = new GeneticAlgorithm(json_decode($jsonString));
 
-if (isset($currentGeneration))
+if (!isset($currentGeneration))
 {
-  // TODO scores
-  //$ga->generateIndividuals();
+  $ga->createIndividuals($dir);
+}
+else
+{
+  $generation = $currentGeneration;
+  $genomes = $ga->json->individuals;
+  $methods = array(0 => array('name'=>'VisitFrequency.get'));
+  $startDate = date('Y-m-d');
+  $endDate = date('Y-m-d');
+  $siteId = 1;
+  $token = '09a21a9bf047682b557d6a0193b12189';
+
+  $scores = PiwikScore::getScores($generation, $genomes, $methods, $startDate, $endDate, $siteId, $token);
+
+  $ga->loadIndividuals($dir);
+  foreach ($ga->population as $individual)
+  {
+    $individual->score = $scores[$individual->genome];
+  }
+
+  $ga->generateIndividuals($dir);
 }
 
 $ga->save($dir);
