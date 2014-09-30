55e4d2568f9a9f7e6db9199469b392ae7d7b6ca2
Bruna Xavier <brunabxsantos@gmail.com>
2014-09-30 13:49:39 -0300

    Removendo cliente

--- client/public/js/default.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function() {
-  jQuery(this).executeGA();
-
-  jQuery('a').click(function() {
-    _paq.push(['trackEvent', 'Link', 'Click', jQuery(this).text()]);
-  });
-});
