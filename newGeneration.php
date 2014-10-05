fc181474ea8d23956f0f4e60edc93e232ddf7395
Bruna Xavier <brunabxsantos@gmail.com>
2014-10-05 04:50:54 -0300

    Criação de uma nova geração requer o código do algoritmo genético

--- newGeneration.php
+++ newGeneration.php
@@ -1,8 +1,5 @@
 <?php
   require 'vendor/autoload.php';
-
-  define('RESOURCES_DIR', dirname(__FILE__) . '/resources/');
-
-  $controller = new GeneticAlgorithmController(RESOURCES_DIR);
-  $controller->execute();
+  $controller = new GeneticAlgorithmController();
+  $controller->createNextGeneration($_GET['code']);
 ?>
