2797e403eff986f1c6831c835bb52a4042a486eb
Bruna Xavier <brunabxsantos@gmail.com>
2014-10-17 11:00:27 -0300

    Testes de aceitação para o fluxo do processo

---
+++ src/test/acceptance/ClientConfigurationAcceptanceTest.php
@@ -0,0 +1,30 @@
+<?php
+include_once 'MySelenium_TestCase.php';
+class ClientConfigurationAcceptanceTest extends MySelenium_TestCase
+{
+  public function testClientConfiguration_afterComplete_mustGoToScheduleNextGenerationStep()
+  {
+    // Arrange
+    $this->login('user1', '123456');
+
+    // Act
+    $this->click(WebDriverBy::id('btn_submit'));
+    $this->waitPresenceOfElement(WebDriverBy::id('appMenuSelected'));
+
+    // Assert
+    $this->assertEquals('Passo 4', $this->text(WebDriverBy::id('appMenuSelected')));
+  }
+
+  private function login($user, $password)
+  {
+    $this->access();
+    $user = 'user1';
+    $password = '123456';
+    $this->write(WebDriverBy::id('txt_user'), $user);
+    $this->write(WebDriverBy::id('txt_password'), $password);
+
+    $this->click(WebDriverBy::id('btn_login'));
+    $this->waitPresenceOfElement(WebDriverBy::id('appMenuSelected'));
+  }
+}
+?>
---
+++ src/test/acceptance/ServerConfigurationAcceptanceTest.php
@@ -0,0 +1,36 @@
+<?php
+include_once 'MySelenium_TestCase.php';
+class ServerConfigurationAcceptanceTest extends MySelenium_TestCase
+{
+  public function testServerConfiguration_afterComplete_mustGoToClientConfigurationStep()
+  {
+    // Arrange
+    $this->login('user1', '123456');
+    $token = 'token123';
+    $versions = '4';
+    $properties = 'h1:class1; h2:class2;';
+
+    // Act
+    $this->write(WebDriverBy::id('txt_token'), $token);
+    $this->write(WebDriverBy::id('txt_versions'), $versions);
+    $this->write(WebDriverBy::id('txt_prop'), $properties);
+    $this->click(WebDriverBy::id('btn_submit'));
+    $this->waitPresenceOfElement(WebDriverBy::id('appMenuSelected'));
+
+    // Assert
+    $this->assertEquals('Passo 3', $this->text(WebDriverBy::id('appMenuSelected')));
+  }
+
+  private function login($user, $password)
+  {
+    $this->access();
+    $user = 'user1';
+    $password = '123456';
+    $this->write(WebDriverBy::id('txt_user'), $user);
+    $this->write(WebDriverBy::id('txt_password'), $password);
+
+    $this->click(WebDriverBy::id('btn_login'));
+    $this->waitPresenceOfElement(WebDriverBy::id('appMenuSelected'));
+  }
+}
+?>
