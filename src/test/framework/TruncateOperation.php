f052442605c49c3478bfd6626ff2d0346a6eca6a
Bruna Xavier <brunabxsantos@gmail.com>
2014-10-17 13:44:05 -0300

    Separando a classe TruncateOperation

---
+++ src/test/framework/TruncateOperation.php
@@ -0,0 +1,11 @@
+<?php
+class TruncateOperation extends PHPUnit_Extensions_Database_Operation_Truncate
+{
+  public function execute(PHPUnit_Extensions_Database_DB_IDatabaseConnection $connection, PHPUnit_Extensions_Database_DataSet_IDataSet $dataSet)
+  {
+    $connection->getConnection()->query("SET foreign_key_checks = 0");
+    parent::execute($connection, $dataSet);
+    $connection->getConnection()->query("SET foreign_key_checks = 1");
+  }
+}
+?>
--- src/test/functional/MyDatabase_TestCase.php
+++ src/test/functional/MyDatabase_TestCase.php
@@ -66,14 +66,4 @@ abstract class MyDatabase_TestCase extends PHPUnit_Extensions_Database_TestCase
     $this->assertTablesEqual($expectedTable, $queryTable);
   }
 }
-
-class TruncateOperation extends PHPUnit_Extensions_Database_Operation_Truncate
-{
-  public function execute(PHPUnit_Extensions_Database_DB_IDatabaseConnection $connection, PHPUnit_Extensions_Database_DataSet_IDataSet $dataSet)
-  {
-    $connection->getConnection()->query("SET foreign_key_checks = 0");
-    parent::execute($connection, $dataSet);
-    $connection->getConnection()->query("SET foreign_key_checks = 1");
-  }
-}
 ?>
